2a61a94f8581deb58ddc898cc50acb66d3dd3187
Terence1991 <cplterencehawes@gmail.com>
2019-02-15 03:06:59 +0000

    routes installed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import {BrowerRouter, Route} from 'react-router-dom'
+
+const PageOne = () => {
+  return (
+    <div>page one</div>
+  )
+}
+
+const PageTwo = () => {
+   return (
+     <div>page two</div>
+   )
+}
 
 
 const App = () => {
   return (
-    <div>APP</div>
+  <div>
+    <BrowerRouter>
+      <div>
+      <Route path="/" exact component={PageOne}/>
+      <Route path="/pagetwp"  component={PageTwo}/>
+      </div>
+    </BrowerRouter>
+  </div>
   )
 }
 
