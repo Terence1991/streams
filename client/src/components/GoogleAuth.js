260f825ff765f9a5f181533aba318264013ce239
Terence1991 <cplterencehawes@gmail.com>
2019-03-22 13:42:33 +0000

    call to auth

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 
 class GoogleAuth extends React.component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+     window.gapi.client.init({
+       clientId: '1025169971631-oj5bh400fi1pugoj0anpdfmi2b4amqk5.apps.googleusercontent.com',
+       scope: 'email'
+     })
+    })
+  }
+
   render() {
     return <div></div>
   }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import GoogleAuth from './GoogleAuth'
 
 const Header = () => {
   return (
     <div>
       <Link to='/'>Steams</Link>
       <div>  
-      <Link to='/'>ALL Streams</Link>
+      <Link to='/'>ALL Streams</Link>\
+      <GoogleAuth/>
       </div>
     </div>
   )
