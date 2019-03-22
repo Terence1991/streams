7373d920f2ed86c660541fdee3d7a72357478843
Terence1991 <cplterencehawes@gmail.com>
2019-03-22 14:12:05 +0000

    user auth flow

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -2,17 +2,33 @@ import React from 'react'
 
 
 class GoogleAuth extends React.component {
+  state = {isSignedIn: null}
+
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
      window.gapi.client.init({
        clientId: '1025169971631-oj5bh400fi1pugoj0anpdfmi2b4amqk5.apps.googleusercontent.com',
        scope: 'email'
+     }).then(() => {
+       this.auth = window.gapi.auth2.getAuthInstance();
+       this.setState({isSignedIn: this.authisSignedIn.get()})
      })
     })
   }
 
+  renderAuthButton() {
+    if(this.state.isSignedIn === null) {
+      return <div> i dont know if we are signed in</div>
+    } else if(this.state.isSignedIn) {
+       return <div> I am signed in</div>
+    } else {
+      return <div> I am not signed in</div>
+    }
+  }
+
   render() {
-    return <div></div>
+    return <div>{this.renderAuthButton}</div>
   }
 }
 
